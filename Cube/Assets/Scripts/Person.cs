11e9fee22a9186be86ece8eea447c8aa46f6be6e
Liyuwang <337255891@qq.com>
2017-10-14 10:28:56 +0800

    add Assets001

---
+++ Cube/Assets/Scripts/Person.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour {
+
+    private string[] identity;
+    private bool isAlive;
+    private float moveSpeed = 1.0f;
+    private CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+        cc = this.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    public void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        cc.Move(new Vector3(x, 0, y)*moveSpeed);
+    }
+
+
+
+    public bool getIsAlive()
+    {
+        return isAlive;
+    }
+    public void setIsAlive(bool isAlive)
+    {
+        this.isAlive = isAlive;
+    }
+    public float getMoveSpeed()
+    {
+        return moveSpeed;
+    }
+    public void setMoveSpeed(float moveSpeed)
+    {
+        this.moveSpeed = moveSpeed;
+    }
+}
